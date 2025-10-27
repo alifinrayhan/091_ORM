30a9e1d19b0baf7648c81cd1a017cc492e95eeca
alifinrayhan <alifinrayhan@gmail.com>
2025-10-27 09:15:03 +0700

    Membuat model komik

---
+++ models/komik.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Komik = sequelize.define('Komik', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+    
+    },
+    author: {
+      type: DataTypes.STRING,
+      
+    },  
+  });
+
+  return Komik;
+}
